14aac3074eafa775525bed03bcef9666177b8662
NedosypI <irinanedosyp@gmx.de>
2022-07-12 15:09:54 +0200

    To-Do

--- script_01.js
+++ script_01.js
@@ -11,15 +11,20 @@
 //application / App
 startApp();
 function startApp(){
-    output(calculator(getNumber(),getNumber(),getOp()));
+    output(calculator(getNumber("1"),getOp(),getNumber("2")));
 }
 
-function getNumber() {
-    return 2;
+function getNumber(numTxt) {
+    const displayStr = "Bitte Zahl " + numTxt + " eingeben";
+    let inputStr =prompt(displayStr);
+    let num = parseInt(inputStr)
+    return num;
 }
 
 function getOp() {
-    return "+";
+    const displayStr ="Bitte korrekten Operator (+ | - | * | :) eingeben";
+    let op = prompt(displayStr);
+    return op;
 }
 
 // module: calculator | tests:
@@ -31,7 +36,7 @@ function getOp() {
 // output(calculator(2,0,":"));
 // output(calculator(2,0,"#!"));
 
-function calculator(a,b,op) {
+function calculator(a,op,b) {
 
     switch (op) {
         case "+":
@@ -100,3 +105,7 @@ function add(a,b) {
 function output(inputData) {
     console.log(inputData)
 }
+
+// TO DO: 
+// output(20) ---> "The result is: " 20
+// output("Error") --> "Error"
