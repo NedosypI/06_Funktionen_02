9e92cd86e7d3b05ac9c40818ad10347a1878df74
NedosypI <irinanedosyp@gmx.de>
2022-07-12 13:00:25 +0200

    module: calculator

--- script_01.js
+++ script_01.js
@@ -3,26 +3,51 @@
 /*
 0. a+b / a-b/ a*b / a/b  // ergebnis c 
 1. Dateneingabe + -überprüfung :
-2. Auswahl Rechenart :
-3. Fkt. Grundrechenarten :
-4. Ausgabe in Konsole : check
+2. Auswahl Rechenart : check!
+3. Fkt. Grundrechenarten :check!
+4. Ausgabe in Konsole : check!
 */
 
+// module: calculator | tests:
+// agreement : "+","-","*",":"
+output(calculator(2,2,"+"));
+output(calculator(2,2,"-"));
+output(calculator(2,2,"*"));
+output(calculator(2,2,":"));
+output(calculator(2,0,":"));
+output(calculator(2,0,"#!"));
+
+function calculator(a,b,op) {
+
+    switch (op) {
+        case "+":
+            return add(a,b); //addition
+        case "-":
+            return subtract(a,b); //subtraction
+        case "*":
+            return multiply(a,b); //multiplication
+        case ":":
+            return divide(a,b); //division
+        default:
+            return "OOPS...Somethig went wrong!";
+    }
+}
+
+
+
 // module: division  a / b |  test:
-output(divide(2,3));
-output(divide(2,-3));
-output(divide(2.1,3.2));
-output(divide(2,0));
+// output(divide(2,3));
+// output(divide(2,-3));
+// output(divide(2.1,3.2));
+// output(divide(2,0));
 
 function divide(a,b) { 
 
     if (b==0) {
         return "Division by 0 not possible!";
     } 
-       return a / b 
-    
+       return a / b  
 }
-
 
 // module: multiplication  a * b |  test:
 // output(multiply(2,3));
